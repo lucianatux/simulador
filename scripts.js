6702c5e452ae2d24c2cfe59c8ce4c66332ab122d
lucianatux <lucianatux@yahoo.com.ar>
2023-03-21 17:47:21 -0300

    pensamientos

--- scripts.js
+++ scripts.js
@@ -3,14 +3,16 @@ const selectEstado = document.getElementById('selectEstado');
 const botonMusica = document.getElementById('botonMusica');
 const botonFrases = document.getElementById('botonFrases');
 const audio = document.getElementById('audio');
+const frase = document.getElementById('frase');
 
 selectEstado.addEventListener('change', cambiarEstado);
 botonMusica.addEventListener('click', reproducirMusica);
-botonFrases.addEventListener('click', mostrarFrases);
+//botonFrases.addEventListener('click', mostrarFrases);
 
 function cambiarEstado() {
     const estado = selectEstado.value;
     imagen.setAttribute('data-estado', estado);
+    mostrarFrases();
   }
 
 function reproducirMusica() {
@@ -22,5 +24,22 @@ function reproducirMusica() {
   }
 
 function mostrarFrases() {
-  // Aquí puedes mostrar las frases de alguna forma en la imagen
-}
+    const estado = selectEstado.value;
+    switch (estado) {
+      case 'neutral':
+        frase.textContent = 'No tengo mucho que decir';
+        break;
+      case 'feliz':
+        frase.textContent = '¡Qué bien me siento!';
+        break;
+      case 'triste':
+        frase.textContent = 'No me siento muy bien';
+        break;
+      case 'enojado':
+        frase.textContent = '¡Esto es inaceptable!';
+        break;
+      default:
+        frase.textContent = '';
+    }
+  }
+  
