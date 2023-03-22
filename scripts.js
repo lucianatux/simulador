d737e95f1e8b6ed2d961d10007777287e70b503e
lucianatux <lucianatux@yahoo.com.ar>
2023-03-22 20:57:08 -0300

    funcion cambiarNivel

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
 const imagen = document.getElementById('imagen');
 const selectEstado = document.getElementById('selectEstado');
+const selectNivel = document.getElementById('selectNivel');
 const botonMusica = document.getElementById('botonMusica');
-const botonFrases = document.getElementById('botonFrases');
+//const botonFrases = document.getElementById('botonFrases');
 const audio = document.getElementById('audio');
 const frase = document.getElementById('frase');
 
 selectEstado.addEventListener('change', cambiarEstado);
+selectNivel.addEventListener('change', cambiarNivel);
 botonMusica.addEventListener('click', reproducirMusica);
 //botonFrases.addEventListener('click', mostrarFrases);
 
 function cambiarEstado() {
     const estado = selectEstado.value;
     imagen.setAttribute('data-estado', estado);
+    console.log(estado);
     mostrarFrases();
+  }
+
+function cambiarNivel() {
+    const nivel = selectNivel.value;
+    imagen.setAttribute('data-nivel', nivel);
+    console.log(nivel);
   }
 
 function reproducirMusica() {
