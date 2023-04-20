824ff88e2a6934e1a45ad6a4f406ce71f779c041
lucianatux <lucianatux@yahoo.com.ar>
2023-04-20 17:35:05 -0300

    mostrarSentidos

--- scripts.js
+++ scripts.js
@@ -2,22 +2,22 @@ const imagen = document.getElementById('imagen');
 const selectEstado = document.getElementById('selectEstado');
 const selectNivel = document.getElementById('selectNivel');
 const botonMusica = document.getElementById('botonMusica');
-//const botonFrases = document.getElementById('botonFrases');
 const audio = document.getElementById('audio');
 const frase = document.getElementById('frase');
 const frase2 = document.getElementById('frase2');
 const frase3 = document.getElementById('frase3');
+const sentidos = document.getElementById('sentidos');
 
 selectEstado.addEventListener('change', cambiarEstado);
 selectNivel.addEventListener('change', cambiarNivel);
 botonMusica.addEventListener('click', reproducirMusica);
-//botonFrases.addEventListener('click', mostrarFrases);
 
 function cambiarEstado() {
     const estado = selectEstado.value;
     imagen.setAttribute('data-estado', estado);
     console.log(estado);
     mostrarFrases();
+    mostrarSentidos();
   }
 
 
@@ -103,4 +103,74 @@ function cambiarNivel() {
   imagen.setAttribute('data-nivel', nivel);
   console.log(nivel);
   cambioPercepcion();
+  mostrarSentidos();
+}
+
+function mostrarSentidos() {
+  const estado = selectEstado.value;
+  const nivel = selectNivel.value;
+  switch (estado + "-" + nivel) {
+    case 'neutral-sueño':
+      console.log('neutralsueño');
+      break;
+    case 'neutral-semisueño':
+      console.log('neutralsemisueño');
+      break;
+    case 'neutral-vigilia':
+      console.log('neutralvigilia');
+      break;
+    case 'neutral-conc-de-si':
+      console.log('neutralconcdesi');
+      break;
+    case 'neutral-conc-objetiva':
+      console.log('neutralobjetiva');
+      break;
+    case 'feliz-sueño':
+      console.log('felizsueño');
+      break;
+    case 'feliz-semisueño':
+      console.log('felizsemisueño');
+      break;
+    case 'feliz-vigilia':
+      console.log('felizvigilia');
+      break;
+    case 'feliz-conc-de-si':
+      console.log('feliz-conc-de-si');
+      break;
+    case 'feliz-conc-objetiva':
+      console.log('felizconc-objetica');
+      break;
+    case 'triste-sueño':
+      console.log('tristesueño');
+      break;
+    case 'triste-semisueño':
+      console.log('tristesemisueño');
+      break;
+    case 'triste-vigilia':
+      console.log('tristevigilia');
+      break;
+    case 'triste-conc-de-si':
+      console.log('triste-conc-de-si');
+      break;
+    case 'triste-conc-objetiva':
+      console.log('tristeconc-objetiva');
+      break;
+    case 'enojado-sueño':
+      console.log('enojadosueño');
+      break;
+    case 'enojado-semisueño':
+      console.log('enojadosemisueño');
+      break;
+    case 'enojado-vigilia':
+      console.log('enojadovigilia');
+      break;
+    case 'enojado-conc-de-si':
+      console.log('enojado-conc-de-si');
+      break;
+    case 'enojado-conc-objetiva':
+      console.log('enojadoconc-objetiva');
+      break;
+    default:
+      frase.textContent = '';
+  }
 }
